ea1a338b029fce1f0dffc545a2fa41d7bc5271b2
Michael Desanker <mdesanker@gmail.com>
2022-02-20 06:21:32 -0500

    Add tests for login component

--- src/components/views/auth/Login.test.tsx
+++ src/components/views/auth/Login.test.tsx
@@ -1,18 +1,22 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
-import { store } from "./store/store";
-import App from "./App";
+import { store } from "../../../store/store";
 import { BrowserRouter as Router } from "react-router-dom";
+import Login from "./Login";
 
-test.skip("full app rendering/navigating", () => {
+test("full app rendering/navigating", () => {
   const { getByText } = render(
     <Provider store={store}>
       <Router>
-        <App />
+        <Login />
       </Router>
     </Provider>
   );
+
+  const title = screen.getAllByText(/Log in/i)[0] as HTMLHeadingElement;
+
+  expect(title).toBeInTheDocument();
 
   expect(getByText(/Welcome back/i)).toBeInTheDocument();
 });
