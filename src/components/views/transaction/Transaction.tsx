356b0138c825e517a8a5463c6e829a47b756a489
Michael Desanker <mdesanker@gmail.com>
2022-02-23 15:25:34 -0500

    Add rough outline of transaction form

--- src/components/views/transaction/Transaction.tsx
+++ src/components/views/transaction/Transaction.tsx
@@ -8,28 +8,74 @@ const Transaction = () => {
   };
 
   return (
-    <main className="main gap-2">
+    <main className="main gap-2 flex flex-col">
       <h1>Add Transaction</h1>
-      <div className="flex w-full px-4">
-        <button
-          className={`bg-gray-200 grow font-medium py-2 duration-200 ${
-            type === "expense" ? "bg-sky-700 text-white" : ""
-          }`}
-          onClick={typeHandler}
-          value="expense"
-        >
-          Expense
-        </button>
-        <button
-          className={`bg-gray-200 grow font-medium py-2 duration-200 ${
-            type === "earning" ? "bg-sky-700 text-white" : ""
-          }`}
-          onClick={typeHandler}
-          value="earning"
-        >
-          Earning
-        </button>
-      </div>
+      <form className="flex flex-col">
+        <div className="flex w-full px-4">
+          <button
+            type="button"
+            className={`bg-gray-200 grow font-medium py-2 duration-200 ${
+              type === "expense" ? "bg-sky-700 text-white" : ""
+            }`}
+            onClick={typeHandler}
+            value="expense"
+          >
+            Expense
+          </button>
+          <button
+            type="button"
+            className={`bg-gray-200 grow font-medium py-2 duration-200 ${
+              type === "earning" ? "bg-sky-700 text-white" : ""
+            }`}
+            onClick={typeHandler}
+            value="earning"
+          >
+            Earning
+          </button>
+        </div>
+        <div className="flex">
+          <div className="flex flex-col w-2/3">
+            <label htmlFor="merchant">
+              {type === "expense" ? "To" : "From"}
+            </label>
+            <input
+              type="text"
+              name="merchant"
+              id="merchant"
+              className="border-b outline-none"
+            />
+          </div>
+          <div className="flex flex-col w-1/3">
+            <label htmlFor="amount">Amount</label>
+            <div className="flex">
+              <p>$</p>
+              <input
+                type="number"
+                name="amount"
+                id="amount"
+                placeholder="00.00"
+                className="appearance-none outline-none m-0 border-b"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col">
+          <label htmlFor="description">Description</label>
+          <input
+            type="text"
+            name="description"
+            id="description"
+            className="border-b outline-none"
+          />
+        </div>
+
+        <select name="category" id="category">
+          <option value="personal">Personal</option>
+          <option value="billsutilities">Bills & Utilities</option>
+          <option value="professionalService">Professional Service</option>
+        </select>
+        <button>Add transaction</button>
+      </form>
     </main>
   );
 };
